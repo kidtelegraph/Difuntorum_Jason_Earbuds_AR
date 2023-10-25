dae75da437c5a8ac3dcd930fb707b3e741bcbe21
kidtelegraph <jxen.dee@gmail.com>
2023-10-24 22:47:39 -0400

    update_code
    fixed AR issues

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const primaryNav = document.querySelector('#navbar');
+const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const detailContainer = document.getElementById("detail-list");
 
